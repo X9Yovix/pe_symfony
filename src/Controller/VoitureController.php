e8199336e17acdc9e9655f3054e02daea87451c9
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-15 22:24:08 +0100

    list of cars

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Voiture;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VoitureController extends AbstractController
 {
-    #[Route('/voiture', name: 'voiture')]
-    public function listeVoitures(): Response
+    #[Route('/voitures', name: 'voitures')]
+    public function listeVoitures(EntityManagerInterface $em): Response
     {
-        return $this->render('voiture/liste_voiture.html.twig', []);
+        $voitures = $em->getRepository(Voiture::class)->findAll();
+        return $this->render('voiture/liste_voiture.html.twig', ['liste_voitures' => $voitures]);
     }
 }
