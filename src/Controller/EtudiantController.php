37a259186f1d1a0aab5a7408caa5bb98845ee31e
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-15 20:13:14 +0100

    display name of student using twig temp

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -14,9 +14,16 @@ class EtudiantController extends AbstractController
         return new Response("Bonjour mes étudiants");
     }
 
-    #[Route('/etudiant/{id}',name:'affichage_etudiant')]
+    /* #[Route('/etudiant/{id}', name: 'affichage_etudiant')]
     public function affichageEtudiant($id): Response
     {
-        return new Response("Bonjour étudiant ayant l'id : ".$id);
+        return new Response("Bonjour étudiant ayant l'id : " . $id);
+    }
+    */
+
+    #[Route('/etudiant/{name}', name: 'etudiant_name')]
+    public function voirNom($name): Response
+    {
+        return $this->render('/etudiant/etudiant.html.twig', ['name' => $name]);
     }
 }
