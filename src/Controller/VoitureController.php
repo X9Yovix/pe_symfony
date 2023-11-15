fd24e078bfeeaa2a9323d8df865a453883ceb962
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-15 23:18:17 +0100

    creating form

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Voiture;
+use App\Form\VoitureFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +16,15 @@ class VoitureController extends AbstractController
     {
         $voitures = $em->getRepository(Voiture::class)->findAll();
         return $this->render('voiture/liste_voiture.html.twig', ['liste_voitures' => $voitures]);
+    }
+
+    #[Route('/voiture/ajouter', name: 'ajouter_voiture')]
+    public function ajouterVoiture(): Response
+    {
+        $voiture = new Voiture();
+        $form = $this->createForm(VoitureFormType::class, $voiture);
+        return $this->render('voiture/ajouter_voiture.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/VoitureFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class VoitureFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('serie', TextType::class, [
+                'label' => 'Serie',
+                'attr' => [
+                    'placeholder' => 'Serie'
+                ]
+            ])
+            ->add('date_mise_circulation', DateTimeType::class, [
+                'label' => 'Date de mise en circulation'
+            ])
+            ->add('modele', TextType::class, [
+                'label' => 'Modele',
+                'attr' => [
+                    'placeholder' => 'Modele'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
